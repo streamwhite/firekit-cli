a06c1ab4564aa1530451fd10ab8e0cbb156c907e
henry he <streamwhite@hotmail.com>
2024-12-18 20:16:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+import { Command } from 'commander';
+import fs from 'fs';
+import process from 'node:process';
+const program = new Command();
+
+program
+  .name('firebase-cli-app')
+  .description('CLI tool to manage Firebase projects and apps')
+  .version('1.0.0');
+
+program
+  .command('setup <projectId> <appName>')
+  .description('Login, create project, create web app, and get SDK config')
+  .action((projectId, appName) => {
+    try {
+      console.log('Logging in to Firebase...');
+      execSync('firebase login', { stdio: 'inherit' });
+
+      const projectsList = JSON.parse(
+        execSync('firebase projects:list --json', { encoding: 'utf-8' })
+      ).result;
+
+      const projectExists = projectsList.some(
+        (project) => project.projectId === projectId
+      );
+      const uniqueProjectId =
+        projectId + '-' + new Date().toISOString().replace(/[:.]/g, '-');
+      if (!projectExists) {
+        console.log(`Creating Firebase project: ${uniqueProjectId}...`);
+        execSync(`firebase projects:create ${uniqueProjectId}`, {
+          stdio: 'inherit',
+        });
+      } else {
+        console.log(
+          `it will create web app with existed Firebase project ${uniqueProjectId}`
+        );
+      }
+
+      console.log(`Creating Firebase web app: ${appName}...`);
+      execSync(
+        `firebase apps:create WEB ${appName} --project ${uniqueProjectId}`,
+        {
+          stdio: 'inherit',
+        }
+      );
+      const appId = JSON.parse(
+        execSync(`firebase apps:list --project ${uniqueProjectId} --json`, {
+          encoding: 'utf-8',
+        })
+      ).result.find((app) => app.displayName === appName).appId;
+
+      console.log('Fetching web app SDK configuration...');
+      // firebase apps:sdkconfig WEB 1:981439365318:web:f8d0990e8d386a1f5b57c7
+      const configOutput = execSync(`firebase apps:sdkconfig WEB ${appId}`, {
+        encoding: 'utf-8',
+      });
+      const regex = /firebase\.initializeApp\((\{[\s\S]*?\})\);/;
+
+      const config = configOutput.match(regex)[1];
+      // save config to a json file
+      fs.writeFileSync('firekit-config.json', config);
+
+      // show the config is saved
+      console.log('🎉🎉🎉SDK configuration saved to firekit-config.json🎉🎉🎉');
+    } catch (error) {
+      console.error('Error during setup:', error.message);
+    }
+  });
+
+program.parse(process.argv);
+
+// doc: add project id default value
+// doc: projectId new project name or existing project id, it will add utc time to the project id, use a meaningful name(no prd test like)
